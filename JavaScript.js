3c4d19d57b2f0c10668eaab54ddb8865ea4f0f2a
ITA5-TN08 <ITA5-TN08@PC0430>
2025-01-24 15:57:25 +0100

    Add project files.

---
+++ JavaScript.js
@@ -0,0 +1,102 @@
+﻿//Variablendefinitionen
+const canvas = document.getElementById("spielfeld");
+const spielfeldrahmen = document.getElementById("spielfeldrahmen");
+const ctx = canvas.getContext("2d");
+const spielsteincontainer = document.getElementById("spielsteincontainer");
+const spielsteine = document.getElementsByClassName("stein");
+const columncontainer = document.getElementById("columnrahmen");
+
+//Elementmodifikationen
+
+canvas.width = spielfeldrahmen.offsetWidth;
+canvas.height = spielfeldrahmen.offsetHeight;
+//canvas.style.position = "absolute";
+//canvas.style.zIndex = "1";
+const fieldWidth = canvas.width / 7;
+const fieldHeight = canvas.height / 6;
+
+//Spielfeldaufbau
+AddGamePieces();
+SpielfeldOverlayHinzufügen();
+
+//Auf Spielfeldelemente zugreifen
+const stein1 = document.getElementById("r20");
+
+
+//EventListener
+stein1.addEventListener("dragstart", onDragStart);
+spielfeldrahmen.addEventListener("dragend", onDragEnd);
+
+for (let i = 0; i < 7; i++)
+{
+    for (let j = 0; j < 6; j++)
+    {
+        DrawField(i, j);
+    }
+}
+function DrawField(x, y)
+{
+    ctx.beginPath();
+    ctx.arc(fieldWidth * x + fieldWidth / 2, fieldHeight * y + fieldHeight / 2, fieldHeight / 2 - 10, 0, 360 * (Math.PI / 180), true);
+    ctx.stroke();
+    ctx.rect(x * fieldWidth, y * fieldHeight, fieldWidth, fieldHeight);
+    ctx.stroke();
+    ctx.fillStyle = "DodgerBlue";
+    ctx.fill();
+    ctx.closePath();
+}
+function AddGamePieces()
+{
+    for (let i = 0; i < 21; i++)
+    {
+        let image = document.createElement("img");
+        image.src = "RoterSteinSeitenansicht.png"
+        image.id = "r" + i;
+        image.setAttribute("draggable", "true");
+        image.width = fieldWidth - 15;
+        spielsteincontainer.appendChild(image);
+
+    }
+}
+
+function onDragStart(e)
+{
+    e.dataTransfer.setData("text/plain", e.target.id);
+    //e.dataTransfer.setData("application/x-moz-node", e.target);
+    //RemoveGamePiece(e.target);
+    //e.target.hidden = "true";
+    //e.dataTransfer.setData("img")
+    //alert("Dragging");
+}
+function onDragEnd(e)
+{
+
+    alert("Success");
+}
+
+function RemoveGamePiece(node)
+{
+    spielsteincontainer.removeChild(node);
+}
+function SpielfeldOverlayHinzufügen()
+{
+    for (let j = 0; j < 6; j++)
+    {
+        let newRow = document.createElement("div");
+        newRow.setAttribute("class", "row");
+        newRow.id = "gamerow" + j;
+        spielfeldrahmen.appendChild(newRow);
+        for (let i = 0; i < 7; i++)
+        {
+            let div = document.createElement("div");
+            let row = document.getElementById("gamerow"+j)
+            div.style.height = fieldHeight;
+            div.style.width = fieldWidth;
+            div.style.backgroundColor = "red";
+            div.id = "slot" + j + i;
+            //div.className = "col-xs-1";
+            row.appendChild(div);
+
+        }
+    }
+}
---
+++ branch.js
@@ -0,0 +1,66 @@
+﻿const parentdiv = document.getElementById("test");
+const canvas = document.getElementById("cnv");
+const ctx = canvas.getContext("2d");
+//fillContainer();
+parentdiv.style.width = "300px";
+parentdiv.style.height = "300px";
+canvas.style.position = "absolute";
+canvas.height = parentdiv.offsetHeight;
+let start;
+//canvas.style.zIndex = "1";
+//animateCircle();
+//const time = new Date();
+//let tS = time.getSeconds();
+//let tMs = time.getMilliseconds();
+//setInterval(animateCircle, 100 );
+window.onload = () =>
+{
+requestAnimationFrame(animateCircle);
+}
+
+//function firstStep()
+//{
+
+//}
+function animateCircle(timestamp)
+{
+    if (start == undefined)
+    {
+        start = timestamp;
+    }
+    let elapsed = timestamp - start;
+    ctx.setTransform(1, 0, 0, 1, 0, 0);
+    ctx.clearRect(0, 0, 300, 300);
+    ctx.translate(50, 50);
+    ctx.beginPath();
+    //ctx.arc(50, Math.min(50 + 450* elapsed / 1000, 200), 30, 0, 2 * Math.PI);
+    ctx.arc(50, 50 +  Math.sin(elapsed * 0.01) * 50, 30, 0, 2 * Math.PI);
+    ctx.stroke();
+    ctx.fillStyle = "red";
+    ctx.fill();
+    requestAnimationFrame(animateCircle);
+
+}
+
+function fillContainer()
+{
+    for (let i = 0; i < 6; i++)
+    {
+        let newDiv = document.createElement("div");
+        newDiv.setAttribute("class", "row");
+        newDiv.id = "row" + i;
+        // newDiv.style.position = "absolute";
+        parentdiv.appendChild(newDiv);
+        for (let j = 0; j < 7; j++)
+        {
+            let newField = document.createElement("div");
+            let parentElement = document.getElementById("row" + i);
+            newField.style.width = "30px";
+            newField.style.height = "30px";
+            newField.style.backgroundColor = "red";
+            //newField.style.position ="relative"
+            //newField.setAttribute("class", "col-xs-1");
+            parentElement.appendChild(newField);
+        }
+    }
+}
